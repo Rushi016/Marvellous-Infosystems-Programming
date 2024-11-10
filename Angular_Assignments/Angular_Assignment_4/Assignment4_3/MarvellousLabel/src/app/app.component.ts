9de86c1337defee6cc338b474f6b40289badf5dd
Rushi016 <rushi.sable016@gmail.com>
2024-11-10 22:38:52 +0530

    Added Angular Assignment 4

---
+++ Angular_Assignments/Angular_Assignment_4/Assignment4_3/MarvellousLabel/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent
+{
+  title = 'MarvellousLabel';
+  username : string =  "";
+  password : string = "";
+  login : string = "";
+
+  public LoginEvent()
+  {
+    this.login = "Logic Successfully !!!";
+  }
+}
