7acb4a3d4814c31f309e54d9f0220bdfac106cf3
Rushi016 <rushi.sable016@gmail.com>
2024-12-01 15:28:45 +0530

    Added Angular Assignment 11

---
+++ Angular_Assignments/Angular_Assignment_11/Assignment11_1/Marvellous/src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { BooksComponent } from './books/books.component';
+import { InvalidComponent } from './invalid/invalid.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+    {
+        path : '',
+        component : WelcomeComponent
+    },
+    {
+        path : 'technologies',
+        component : TechnologiesComponent
+    },
+    {
+        path : 'books',
+        component : BooksComponent
+    },
+    {
+        path : '**',
+        component : InvalidComponent
+    }
+];
+
+@NgModule(
+    {
+        imports : [RouterModule.forRoot(routes)],
+        exports : [RouterModule]
+    }
+)
+
+export class AppRoutingModule{}
+
